6bfc8d61c4180d78d6d8b7e4fb3a1d8548e87d98
Nido89 <syednaveedjalal@gmail.com>
2020-04-10 16:03:24 +0300

    Chatroom with Chatmessage Template added

--- index.php
+++ index.php
@@ -48,7 +48,7 @@
   <body>
     <nav class="navbar navbar-light" style="background-color: #F4D03F  ;">
   <!-- Navbar content -->
-  <a class="navbar-brand" href="#">SeceretChat</a>
+  <a class="navbar-brand" href="#">Welcome to a SeceretChatRooms.fi Web application service for anyone.Here you can create seceret chat rooms for free to chat annonymously with anyone or your friends and family.</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -155,41 +155,12 @@
       <small class="d-block mb-3 text-muted">&copy; 2017-2019</small>
     </div>
     <div class="col-6 col-md">
-      <h5>Features</h5>
-      <ul class="list-unstyled text-small">
-        <li><a class="text-muted" href="#">Cool stuff</a></li>
-        <li><a class="text-muted" href="#">Random feature</a></li>
-        <li><a class="text-muted" href="#">Team feature</a></li>
-        <li><a class="text-muted" href="#">Stuff for developers</a></li>
-        <li><a class="text-muted" href="#">Another one</a></li>
-        <li><a class="text-muted" href="#">Last time</a></li>
-      </ul>
-    </div>
-    <div class="col-6 col-md">
-      <h5>Resources</h5>
-      <ul class="list-unstyled text-small">
-        <li><a class="text-muted" href="#">Resource</a></li>
-        <li><a class="text-muted" href="#">Resource name</a></li>
-        <li><a class="text-muted" href="#">Another resource</a></li>
-        <li><a class="text-muted" href="#">Final resource</a></li>
-      </ul>
-    </div>
-    <div class="col-6 col-md">
-      <h5>Resources</h5>
-      <ul class="list-unstyled text-small">
-        <li><a class="text-muted" href="#">Business</a></li>
-        <li><a class="text-muted" href="#">Education</a></li>
-        <li><a class="text-muted" href="#">Government</a></li>
-        <li><a class="text-muted" href="#">Gaming</a></li>
-      </ul>
-    </div>
-    <div class="col-6 col-md">
+      
       <h5>About</h5>
       <ul class="list-unstyled text-small">
-        <li><a class="text-muted" href="#">Team</a></li>
-        <li><a class="text-muted" href="#">Locations</a></li>
-        <li><a class="text-muted" href="#">Privacy</a></li>
-        <li><a class="text-muted" href="#">Terms</a></li>
+        <li><a class="text-muted" href="#">Home</a></li>
+        <li><a class="text-muted" href="#">About</a></li>
+        <li><a class="text-muted" href="#">Contact</a></li>
       </ul>
     </div>
   </div>
